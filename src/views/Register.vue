49ef498e1fd98e0cb4ba7b9f491cba5c485431d2
unknown <nikolai.klevanski@gmail.com>
2021-04-13 22:47:29 +0300

    Add Register compon template

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,15 +3,38 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-sx-center">Sign Up</h1>
+          <h1 class="text-xs-center">Sign Up</h1>
           <p class="text-xs-center">
             <router-link :to="{name: 'login'}">Need an account?</router-link>
           </p>
-          Valid errors
+          <ul class="error-messages">
+            <li>Valid errors</li>
+          </ul>
           <form @submit.prevent="onSubmit">
             <fieldset class="form-group">
-              <input type="text" class="form-control form-control-lg">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Your Name"
+              />
             </fieldset>
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign Up
+            </button>
           </form>
         </div>
       </div>
@@ -21,6 +44,11 @@
 
 <script>
 export default {
-  name: 'McRegister'
-}
+  name: 'McRegister',
+  methods: {
+    onSubmit() {
+      console.log('Form submitted');
+    }
+  }
+};
 </script>
